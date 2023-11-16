e8241799a734e5ba430b0f16e620d927b722b215
S-BARATH-KUMAR <95169718+S-BARATH-KUMAR@users.noreply.github.com>
2023-11-16 16:42:39 +0530

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const mongoURI = "mongodb://localhost:27017/HealthcareDApp"
-
-const connectToMongo = () =>{
-    mongoose.connect(mongoURI, {useNewUrlParser: true})
-}
-
-module.exports = connectToMongo;
